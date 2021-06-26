16d6bc7368d5cea8b2fcd7fc0cecd8d948d356d2
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-06-26 17:25:43 +0200

    fix up spelling in teapot path

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const PORT = 3000;
 
-app.get('/', function middleware(req, res) {
-	res.send('Hello Janusz!');
+app.get("/", function middleware(req, res) {
+  res.send("Hello Janusz!");
 });
 
-app.get('/teapot', function middleware(req, res) {
-	res.status(418)
-	res.setHeader("X-Teapot", "white")
-	res.send('I am tepot!');
+app.get("/teapot", function middleware(req, res) {
+  res.status(418);
+  res.setHeader("X-Teapot", "yes");
+  res.send("I'm a teapot!");
 });
 
 app.listen(PORT, function logger() {
-	console.log(`App running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
