7d6c3708cb25f96ba49a62dc1bcacf00a2bb9aae
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-06-26 19:28:07 +0200

    seperate chat and app

--- app.js
+++ app.js
@@ -1,106 +1,17 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+var express = require("express");
+var app = express();
+const PORT = 3000;
 
-const WebSocketServer = require("ws").Server;
-const server = require("http").createServer(app);
-const wss = new WebSocketServer({ server });
-const port = 3000;
-
-// Serve js files
-app.use("/js", express.static(path.join(__dirname, "ui/js/")));
-// Serve css files
-app.use("/css", express.static(path.join(__dirname, "ui/css/")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/ui/html/index.html"));
+app.get("/", function middleware(req, res) {
+  res.send("Hello Janusz!");
 });
 
-let isKozikCached = false;
-
-function isKozik(query) {
-  if (!isKozikCached) {
-    isKozikCached = query.toLowerCase().includes("yes");
-  }
-
-  return isKozikCached;
-}
-
-function handleQuery(query, cb) {
-  if (!isKozik(query)) {
-    cb("Are you Kozik?");
-
-    return;
-  }
-
-  cb("hell yeah buddy!");
-}
-
-/**
- * Client Counter
- * Count the number of active connections
- * @type {Number}
- */
-let cc = 0;
-
-wss.on("connection", function connection(ws) {
-  console.log("client connections: ", ++cc);
-
-  ws.on("message", function incoming(message) {
-    try {
-      const { payload, type } = JSON.parse(message);
-      switch (type) {
-        case "query":
-          handleQuery(payload, (response) => {
-            ws.send(
-              JSON.stringify({ type: "queryResponse", payload: response })
-            );
-          });
-          return;
-        default:
-          console.log(message);
-      }
-    } catch (e) {
-      console.error("Error from message: ", e);
-    }
-  });
-
-  // Send welcome message on each connection
-  if (ws.readyState === ws.OPEN) {
-    ws.send(
-      JSON.stringify({ type: "connected", payload: "Welcome, are you Kozik?!" })
-    );
-  }
-
-  ws.on("close", function close() {
-    --cc;
-    if (cc === 0) {
-      clearInterval(pingInterval);
-    }
-    console.log("disconnected");
-  });
-
-  ws.on("error", function error() {
-    --cc;
-    console.log("error");
-  });
+app.get("/teapot", function middleware(req, res) {
+  res.status(418);
+  res.setHeader("X-Teapot", "yes");
+  res.send("I'm a teapot!");
 });
 
-const pingPayload = JSON.stringify({ type: "ping" });
-// Keep the connection alive
-let pingInterval = setInterval(() => {
-  wss.broadcast(pingPayload);
-}, 1 * 5000);
-
-/**
- * Broadcast data to all connected clients
- * @param  {Object} data
- * @void
- */
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    client.send(data);
-  });
-};
-
-server.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(PORT, function logger() {
+  console.log(`App running on port ${PORT}`);
+});
