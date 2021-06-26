78c93c08592bf12bacdd2fc36df1e63be45c831f
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-06-26 17:22:34 +0200

    add i am teapot path

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', function middleware(req, res) {
 	res.send('Hello Janusz!');
 });
 
+app.get('/teapot', function middleware(req, res) {
+	res.status(418)
+	res.setHeader("X-Teapot", "white")
+	res.send('I am tepot!');
+});
+
 app.listen(PORT, function logger() {
 	console.log(`App running on port ${PORT}`);
 });
